809650386e8bc14b920260bf735bcc0789ccfbc3
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2023-02-11 00:08:05 -0300

    Lesson 11

--- midware/authorize.js
+++ midware/authorize.js
@@ -2,7 +2,7 @@ const verifyLogin = (req, res, next) => {
     if(!req.oidc.isAuthenticated()) {
         res.status(401).send(
             {"message": "You are not login",
-            "login" : "Copy and paste this link on your URL bar: http://localhost:3000/login and press enter"}
+            "login" : "Copy and paste this link on your URL bar: https://antonio-david-tejada-villalon-l09-13.onrender.com/login and press enter"}
         );
     }
     next();
